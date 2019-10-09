5b865caaef1ff6df3deb9dd35dbd2885355a4bd6
Vlad Adomnicai <vlad.adomnicai@gmail.com>
2019-10-09 12:39:51 +0300

    parser: allow training of player1 and player2 independently to be used in mandarkcomp

--- tools/parser.py
+++ tools/parser.py
@@ -63,8 +63,12 @@ def teach(player, moves: Connect4Moves):
             player.teach(move[0], move[1], move[2])
 
 
-def run(player, fname, boost_win_move: float = 0):
-    os.chdir(os.path.dirname(__file__))
+def is_first_player(state) -> bool:
+    return np.sum(np.fromstring(state, dtype=int, sep=' ').reshape(1, 6, 7)) == 0.0
+
+
+def run(player1, player2, fname, boost_win_move: float = 0):
+    # os.chdir(os.path.dirname(__file__))
     # print(os.getcwd())
     with open(fname) as fp:
         match_lines = []
@@ -74,7 +78,10 @@ def run(player, fname, boost_win_move: float = 0):
                 if match_lines:
                     moves = Connect4Moves(boost_win_move=boost_win_move)
                     moves.parse_dump(match_lines)
-                    teach(player, moves)
+                    if is_first_player(match_lines[0]):
+                        teach(player1, moves)
+                    else:
+                        teach(player2, moves)
                     # rewards[1-reward] += 1
                 matches += 1
                 match_lines = []
@@ -85,34 +92,56 @@ def run(player, fname, boost_win_move: float = 0):
         if match_lines:
             moves = Connect4Moves()
             moves.parse_dump(match_lines)
-            teach(player, moves)
+            if is_first_player(match_lines[0]):
+                teach(player1, moves)
+            else:
+                teach(player2, moves)
 
 
 def main():
     # Load model first
     env: ConnectFourEnv = gym.make("ConnectFour-v0")
-    model = 'mandark'
 
-    module = importlib.import_module(model)
-    class_ = getattr(module, model.capitalize())
-    player = class_(env, name=model.capitalize())
+    model1 = 'mandarka'
+    module = importlib.import_module(model1)
+    class_ = getattr(module, model1.capitalize())
+    player1 = class_(env, name=model1.capitalize())
 
-    files = [f for f in glob.glob("**/*.csv", recursive=True)]
-    for f in files:
-        print(f"Loading {f}")
-        if 'minmax' in f:
-            print("Boosting neutral moves")
-            run(player, f"parser/{f}", 0.2)
-        else:
-            run(player, f"parser/{f}", 0.0)
+    model2 = 'mandarkb'
+    module = importlib.import_module(model2)
+    class_ = getattr(module, model2.capitalize())
+    player2 = class_(env, name=model2.capitalize())
 
-        player.save_model()
+    # player2 = player1
+
+    for f in os.listdir('./parser'):
+        if not os.path.isdir('./parser/'+f) and 'csv' in f:
+            print(f"Loading {f}")
+            if 'minmax' in f:
+                print("Boosting neutral moves")
+                run(player1, player2, f"./parser/{f}", 0.2)
+            else:
+                run(player1, player2, f"./parser/{f}", 0.0)
+            player1.save_model()
+            player2.save_model()
+
+    # files = [f for f in glob.glob("**/*.csv", recursive=True)]
+    # for f in files:
+    #     print(f"Loading {f}")
+    #     if 'minmax' in f:
+    #         print("Boosting neutral moves")
+    #         run(player1, player2, f"parser/{f}", 0.2)
+    #     else:
+    #         run(player1, player2, f"parser/{f}", 0.0)
+    #
+    #     player1.save_model()
+    #     player2.save_model()
 
     # for f in ['parser/minmax_vs_random_depth3.csv', 'parser/minmax_vs_deedee_train_0.csv', 'parser/minmax_vs_cnn_173.csv']:
     #     run(player, f)
 
-    player.save_model()
-
+    # player1.save_model()
+    # player2.save_model()
 
 if __name__ == "__main__":
     main()
