43a35972261cd655ef31e444733b0b062835e49c
Pablo Lubitz <13udha666@gmail.com>
2020-03-03 03:07:16 +0100

    Plot für Reward

--- examples/plot.py
+++ examples/plot.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-for i in range(18):
-    pass
+results_path = './results/'
+plot_filename = results_path + 'reward_plot.png'
+
+reward_function = []
+for i in range(22):
+    if i > 3:
+        reward_function.append(1/(i-3))
+
+plt.plot([4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],reward_function)
+plt.plot([4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],np.negative(reward_function))
+plt.plot(21,0,'o',markersize=2)
+plt.xlabel('Züge')
+plt.ylabel('Reward')
+plt.xticks(np.arange(4, 22, step=1))
+# plt.show()
+plt.savefig(plot_filename)
